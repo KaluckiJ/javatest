ccd1de3f710cf7cc9f13ea276d13834393890897
Joe Kalucki <joekalucki@gmail.com>
2015-02-10 20:35:47 -0500

    add js arrays

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var states = ["New Jersey","Maine", "Pennsylvania", "California", "Illinois", "Oregon", "Texas", "Tennessee", "Arizona", "Wyoming", "Montana", "South Dakota", "North Dakota", "Maryland", "Vermont", "New York", "Delaware", "Florida", "North Carolina", "Arkansas",];
+alert(states); 
+alert(states.sort());
+alert("I have been to" + " " + states.length + " " + "states.");
+alert("The second state in my array is" + " " + states[1]);
+alert("The first two states in my array are" + " " + states[1] + " " + "and" + " " + states[2]);
+console.log(states.push("New Mexico"));
+alert(states.sort());
